dc1ac1e2585b88731e4b92f3dad76165c8641413
hackthon99 <hackathonemail99@gmail.com>
2025-04-21 19:17:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
-from fastapi import FastAPI # for creating the FastAPI app
-from pydantic import BaseModel # for data validation and serialization 
-#(it helps in formation of input and output data models for the API)
-from typing import List #for type hinting
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
+from pymongo import MongoClient
+# from pymongo import ServerApi
+from urllib.parse import quote_plus # just to make the url correct
+
+
+username = quote_plus("KunalBavdhane") # Use quote_plus to encode special characters in the username
+password = quote_plus("Kunal12345%") # Use quote_plus to encode special characters in the password
+uri = f"mongodb+srv://{username}:{password}@cluster0.2r5vfqe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+client = MongoClient(uri) # creating a client to connect to the MongoDB database
+database = client["TEST_DATABASE"] # creating a database object to connect to the database
+collection = database["TEST_COLLECTION"] # creating a collection object to connect to the collection in the database
+# for document in collection.find():
+#     print(document)
+collection.insert_one({"name": "Kunal", "age": 23}) # inserting a document into the collection (just for testing purpose)
 
 app = FastAPI() # creating an instance of FastAPI (creating the app)
 
-# creating a class with help of BaseModel for the input data model
-
-class Tea(BaseModel):
-    id : int # id of the tea
-    name : str # name of the tea
-    origin : str # origin of the tea
-    
-
-# teas  = [] # list to store the tea data
-
-# here a teas list with basemodel
-
-teas : list[Tea] = [] # this means that teas is a list of Tea class objects (it will sotre the tea data in the form of class objects with index  0,1,2...)
-# teas is the name of the list and list[Tea] is the type of the list (like it will store which type of data)
-# so in above example it is storing list of list[Tea] (which is a class object)
-
 @app.get("/") # defining the root endpoint ("/") of the API
-# the functions below it will run when on api calls the root with "/" 
-def read_root(): # defining the function to handle the root endpoint
+def read_root():
     # this function will return a welcome message when the root endpoint is called
-    return {"message": "Welcome to the Tea API!"}
+    return {list(collection.find())} # send all collection in the form of list means our whole mongodb data
+
+# @app.post("/insert")
+# async def insert(request : Request):
+#     body = await request.json()
+#     collection.insert_one(body) # inserting the document into the collection
+#     return {"message": "Document inserted successfully :{body}"} 
+    
